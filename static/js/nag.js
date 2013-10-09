5f25054a498491aaa8877620b92b4f1c09c195d2
Timothy Cochran <timothy.cochran@gmail.com>
2013-10-09 15:44:42 +0300

    show notification

--- static/js/nag.js
+++ static/js/nag.js
@@ -35,11 +35,20 @@ $(".clickme").click(function() {
     else {
       console.log("Notifications are not supported for this Browser/OS version yet.");
     }
+    console.log(window.webkitNotifications.checkPermission())
 
     if (window.webkitNotifications.checkPermission() == 0) { // 0 is PERMISSION_ALLOWED
         // function defined in step 2
-        window.webkitNotifications.createNotification(
-            'icon.png', 'Notification Title', 'Notification content...');
+        
+        var notification = window.webkitNotifications.createNotification(
+          "http://www.google.com/images/logo.png", // icon url - can be relative
+          "Google", // notification title
+          "is the best search engine. Click to find out more"  // notification body text
+        );
+
+        console.log(notification);
+        // Show the notification, I'm assuming notifications are supported and allowed
+        notification.show();
       } else {
         window.webkitNotifications.requestPermission();
       }
@@ -51,8 +60,15 @@ $(".clickme").click(function() {
 document.querySelector('.clickme2').addEventListener('click', function() {
   if (window.webkitNotifications.checkPermission() == 0) { // 0 is PERMISSION_ALLOWED
     // function defined in step 2
-    window.webkitNotifications.createNotification(
-        '', 'Notification Title', 'Notification content...');
+   var notification = window.webkitNotifications.createNotification(
+          "", // icon url - can be relative
+          "", // notification title
+          "is the best search engine. Click to find out more"  // notification body text
+        );
+
+        console.log(notification);
+        // Show the notificat
+
   } else {
     window.webkitNotifications.requestPermission();
   }
